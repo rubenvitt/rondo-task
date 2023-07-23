5dd915a3c4ae2b8c26d54be69f09c81f084aab30
Ruben Vitt <dev@rubeen.dev>
2023-07-23 18:12:41 +0200

    Update eslint packages and resolutions details in pnpm-loc.yaml
    Added eslint-config-airbnb-typescript to the eslint packages. Also removed an unnecessary space in resolution details that were causing issues with the integrity of several installed packages. This was done to enable support for TypeScript in our eslint configuration and to fix a potential bug in the resolution mechanism.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Link from 'next/link';
 import React from 'react';
+import Button from '@atoms/Button';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/app">Go to app</Link>
+      <Button href="/app">Go to app</Button>
     </main>
   );
 }
--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import Link from 'next/link';
+
 interface HrefProp {
   href: string;
   action?: never;
@@ -8,6 +11,26 @@ interface ActionProp {
   href?: never;
 }
 
-export function Button() {
-  return <button></button>;
+type Props = React.PropsWithChildren<HrefProp | ActionProp>;
+
+export default function Button({ action, children, href }: Props) {
+  if (href) {
+    return (
+      <Link
+        className="rounded-md bg-white px-2.5 py-1.5 text-sm font-medium text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-primary-500"
+        href={href}
+      >
+        {children}
+      </Link>
+    );
+  }
+  return (
+    <button
+      onClick={action}
+      type="button"
+      className="rounded-md bg-white px-2.5 py-1.5 text-sm font-medium text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-primary-500"
+    >
+      {children}
+    </button>
+  );
 }
